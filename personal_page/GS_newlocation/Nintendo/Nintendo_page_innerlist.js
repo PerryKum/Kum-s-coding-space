e839aba40fe8daa9133930dccbd2e4a538048a85
PerryKum <102568248+PerryKum@users.noreply.github.com>
2022-04-22 22:34:29 +0800

    Add files via upload
    Learned how to use swiper.js in 24hours andactualized it on the "WIIUgames" part. That's a huge breakthrough!

---
+++ personal_page/GS_newlocation/Nintendo/Nintendo_page_innerlist.js
@@ -0,0 +1,8 @@
+var game_swiper_for_WIIU = new Swiper('#game_swiper_for_WIIU',{
+    navigation: {
+        nextEl: '#WIIUnext',
+        prevEl: '#WIIUprev',
+    },
+    spaceBetween: 30.8,
+    slidesPerView: 6,
+});
