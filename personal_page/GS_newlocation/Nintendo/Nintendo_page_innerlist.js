b0ccd3bc39c9665bd45a4632c397c9345da8f603
PerryKum <102568248+PerryKum@users.noreply.github.com>
2022-04-23 16:59:57 +0800

    Add files via upload
    Finished the 3DSLL part.

---
+++ personal_page/GS_newlocation/Nintendo/Nintendo_page_innerlist.js
@@ -0,0 +1,26 @@
+var game_swiper_for_NSL = new Swiper('#game_swiper_for_NSL',{
+    navigation: {
+        nextEl: '#NSLnext',
+        prevEl: '#NSLprev',
+    },
+    spaceBetween: 52.4,
+    slidesPerView: 6,
+});
+
+var game_swiper_for_WIIU = new Swiper('#game_swiper_for_WIIU',{
+    navigation: {
+        nextEl: '#WIIUnext',
+        prevEl: '#WIIUprev',
+    },
+    spaceBetween: 30.8,
+    slidesPerView: 6,
+});
+
+var game_swiper_for_3DSLL = new Swiper('#game_swiper_for_3DSLL',{
+    navigation: {
+        nextEl: '#N3DSLLnext',
+        prevEl: '#N3DSLLprev',
+    },
+    spaceBetween: 42,
+    slidesPerView: 5,
+});
